2a56d470b16363e5b7b3dda4336caf6c3283178d
ibrahim <alfa2563@gmail.com>
2017-06-12 17:30:29 +0000

    run these

---
+++ resources/views/alerts/success.blade.php
@@ -0,0 +1,8 @@
+@if(session('message'))
+
+    <div class="alert alert-success" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-check"></i> Success!</h4>
+        <p>{{session('message')}}</p>
+    </div>
+@endif
